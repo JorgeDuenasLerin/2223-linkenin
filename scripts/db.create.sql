97d2e9aaacb598222141907f18744bd2f2fd0c21
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2023-01-11 20:59:37 +0100

    Comienzo detalle

--- scripts/db.create.sql
+++ scripts/db.create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS usuarios;
 CREATE TABLE usuarios (
     id int auto_increment PRIMARY KEY,
     nombre VARCHAR(255),
@@ -7,10 +8,11 @@ CREATE TABLE usuarios (
     descripcion TEXT
 );
 
+DROP TABLE IF EXISTS tokens;
 CREATE TABLE tokens (
     id int auto_increment PRIMARY KEY,
     id_usuario int,
-    valor VARCHAR(255)
+    valor VARCHAR(255),
     expiracion DATETIME,
     CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
